52676e5f3afec70a15eec7952e9a9c8162743530
elDone <diego-o.o-@hotmail.com>
2025-07-02 17:27:54 -0500

    create fragments

--- app/src/main/java/com/idone/tiendavirtualapp/Vendedor/MainActivityVendedor.kt
+++ app/src/main/java/com/idone/tiendavirtualapp/Vendedor/MainActivityVendedor.kt
@@ -1,15 +1,84 @@
 package com.idone.tiendavirtualapp.Vendedor
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import com.idone.tiendavirtualapp.R
+import com.idone.tiendavirtualapp.Vendedor.Buttom_Nav_Fragments_Vendedor.FragmentMisProductosV
+import com.idone.tiendavirtualapp.Vendedor.Buttom_Nav_Fragments_Vendedor.FragmentOrdenesV
+import com.idone.tiendavirtualapp.Vendedor.Nav_Fragments_Vendedor.FragmentInicioV
+import com.idone.tiendavirtualapp.Vendedor.Nav_Fragments_Vendedor.FragmentMiTiendaV
+import com.idone.tiendavirtualapp.Vendedor.Nav_Fragments_Vendedor.FragmentReseniaV
+import com.idone.tiendavirtualapp.databinding.ActivityMainVendedorBinding
 
-class MainActivityVendedor : AppCompatActivity() {
+class MainActivityVendedor : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding : ActivityMainVendedorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_vendedor)
+        binding = ActivityMainVendedorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        
+        replaceFragment(FragmentInicioV())
+        binding.navigationView.setCheckedItem(R.id.op_inicio_v)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.navFragment, fragment)
+            .commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.op_inicio_v->{
+                replaceFragment(FragmentInicioV())
+            }
+            R.id.op_mi_tienda_v->{
+                replaceFragment(FragmentMiTiendaV())
+            }
+            R.id.op_resenia_v->{
+                replaceFragment(FragmentReseniaV())
+            }
+            R.id.op_cerrar_sesion_v->{
+                Toast.makeText(applicationContext, "Saliste de la aplicacion", Toast.LENGTH_SHORT).show()
+            }
+            R.id.op_mis_productos_v->{
+                replaceFragment(FragmentMisProductosV())
+            }
+            R.id.op_mis_ordenes_v->{
+                replaceFragment(FragmentOrdenesV())
+            }
+
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
---
+++ app/src/main/java/com/idone/tiendavirtualapp/Vendedor/Nav_Fragments_Vendedor/FragmentInicioV.kt
@@ -0,0 +1,20 @@
+package com.idone.tiendavirtualapp.Vendedor.Nav_Fragments_Vendedor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.idone.tiendavirtualapp.R
+
+
+class FragmentInicioV : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
