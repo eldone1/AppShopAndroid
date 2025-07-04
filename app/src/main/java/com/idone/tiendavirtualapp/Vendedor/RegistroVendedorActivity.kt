1327a108bc0a44779be4ea2e87e250cd510baa23
elDone <diego-o.o-@hotmail.com>
2025-07-03 22:19:15 -0500

    create form registroV

---
+++ app/src/main/java/com/idone/tiendavirtualapp/Vendedor/RegistroVendedorActivity.kt
@@ -0,0 +1,12 @@
+package com.idone.tiendavirtualapp.Vendedor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.idone.tiendavirtualapp.R
+
+class RegistroVendedorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_vendedor)
+    }
+}
